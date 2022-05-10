70dcc7b7fe2615eb1357146e83a6609e87a09500
omniFatal <matthew.t.karyadi@gmail.com>
2022-05-09 19:48:29 -0700

    Sets up tableview data and settings button

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -6,27 +6,39 @@
 //
 
 import UIKit
-//class TableViewDelegate : NSObject, UITableViewDataSource, UITableViewDelegate {
-//    let data : [String] = [
-//    "Math","Marvel Super Heros", "Science"
-//    ]
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return data.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        <#code#>
-//    }
-//
-//
-//}
+
+
+class TableViewDelegateAndDataSource : NSObject, UITableViewDataSource, UITableViewDelegate {
+    weak var vc : UIViewController?
+    
+    let data : [String] = [
+    "Math","Marvel Super Heros", "Science"
+    ]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "topic", for: indexPath)
+        
+        cell.textLabel!.text = data[indexPath.row]
+        
+        return cell
+    }
+
+
+}
 
 
 
 class ViewController: UIViewController {
     
+    var tableViewDelegateAndDataSource = TableViewDelegateAndDataSource()
     
     @IBAction func settingsPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Settings", message: "Settings go here.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true)
     }
     
     @IBOutlet weak var quizTable: UITableView!
@@ -34,6 +46,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableViewDelegateAndDataSource.vc = self
+        quizTable.dataSource = tableViewDelegateAndDataSource
+        quizTable.delegate = tableViewDelegateAndDataSource
     }
 
 
