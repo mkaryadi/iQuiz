82d8fddb4df0917e862957cbda0ce1aa850d7a84
omniFatal <matthew.t.karyadi@gmail.com>
2022-05-09 19:33:40 -0700

    Hooks up settings button and table outlet. Table delegate commented out for now

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -6,9 +6,31 @@
 //
 
 import UIKit
+//class TableViewDelegate : NSObject, UITableViewDataSource, UITableViewDelegate {
+//    let data : [String] = [
+//    "Math","Marvel Super Heros", "Science"
+//    ]
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return data.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//
+//
+//}
+
+
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBAction func settingsPressed(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var quizTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
